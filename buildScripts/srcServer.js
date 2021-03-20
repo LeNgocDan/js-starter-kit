fc1dcdcc87029bd08eb9013bd6f73c942b2f3f10
LeNgocDan <Linuss1908@gmail.com>
2021-03-21 02:06:03 +0700

    fix bugs

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,17 +2,24 @@
 var express = require('express');
 var path = require('path');
 var open = require('open');
+// var webpack = require('webpack');
+// var config = require('../webpack.config.dev');
 
 var port = 3000;
-
 var app = express();
+// var compiler = webpack(config);
 
-app.get('/', function(req, res) {
+// app.use(require('webpack-dev-middleware')(compiler, {
+//     noInfo: true,
+//     publicPath: config.output.publicPath
+//   }));
+
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
-  if(err) {
+app.listen(port, function (err) {
+  if (err) {
     console(err);
   } else {
     open('http://localhost:' + port);
