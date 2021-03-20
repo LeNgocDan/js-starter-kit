a3035752cce19284841bb32fb52a215f837d3f23
LeNgocDan <Linuss1908@gmail.com>
2021-03-21 02:33:35 +0700

    update code

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,12 @@
 
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 // var webpack = require('webpack');
 // var config = require('../webpack.config.dev');
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
 // var compiler = webpack(config);
 
 // app.use(require('webpack-dev-middleware')(compiler, {
