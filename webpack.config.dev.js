0775e322f83755483ac485497226ed5fcbdc8762
ngcdan <linuss1908@gmail.com>
2021-04-21 17:21:39 +0700

    config webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,20 @@
+import path from 'path';
+
+export default {
+  debug: true,
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+};
