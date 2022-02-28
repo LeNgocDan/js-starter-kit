5694452e890e939aa84ed05eaed6d9cfad1e38c6
ngcdan <linuss1908@gmail.com>
2022-02-28 16:27:50 +0700

    clean code

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,4 @@
 import { expect } from "chai";
-import jsdom from "jsdom";
-import fs from "fs";
 
 describe("Our first test", () => {
   it("should pass", () => {
@@ -8,13 +6,3 @@ describe("Our first test", () => {
   });
 });
 
-describe("index.html", () => {
-  it("should have h1 that says Users", () => {
-    const index = fs.readFileSync("./src/index.html", "utf-8");
-    const { JSDOM } = jsdom;
-    const dom = new JSDOM(index);
-    const h1 = dom.window.document.getElementsByTagName("h1")[0];
-    expect(h1.innerHTML).to.equal("Users");
-    dom.window.close();
-  });
-});
