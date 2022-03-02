e0c5ace4019ef1a98ec37fef185226e56a3c6696
ngcdan <linuss1908@gmail.com>
2022-03-02 22:33:17 +0700

    update confix

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -9,19 +9,6 @@ const app = express();
 app.use(express.static("dist"));
 app.use(compression());
 
-/*
-app.get("*", function (_req, res) {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
-});
-*/
-
-app.get("/users", (_req, res) => {
-  res.json([
-    { "id": 1, "firstName": "Le Ngoc", "lastName": "Dan", "email": "dan@gmail.com" },
-    { "id": 2, "firstName": "Le Quang", "lastName": "Nhat", "email": "nhat@gmail.com" }
-  ]);
-})
-
 app.get('/', function (_req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,16 +1,23 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default {
   mode: "development",
   devtool: "eval-source-map",
-  entry: "./src/index.js",
+  entry: {
+    main: path.resolve(__dirname, "src/index"),
+    vendor: path.resolve(__dirname, "src/vendor"),
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
-    filename: "bundle.js",
+    filename: "[name].[chunkhash].js",
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
+    }),
     // Create HTML file that includes reference to bundled JS.
     new HtmlWebpackPlugin({
       template: "src/index.html"
@@ -19,7 +26,7 @@ export default {
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
     ],
   },
 };
