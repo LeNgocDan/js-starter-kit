238132092706f81b9b0d9bd7208b6f7c58f1c55b
ngcdan <linuss1908@gmail.com>
2022-12-20 20:23:19 +0700

    update css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './scss/index.scss';
 import xsmb from './assets/xsmb.jpg';
 console.log('test bundle splitting!');
-// global.document.getElementsByClassName('card-img-top').src = xsmb;
 const cardImgs = global.document.getElementsByClassName('card-img-top');
 console.log(cardImgs);
-cardImgs.forEach(img => img.src = xsmb);
+cardImgs.forEach(imgEle => {
+  console.log(imgEle);
+  imgEle.src = xsmb;
+});
+
