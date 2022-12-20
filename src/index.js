5cd5a0629052c5a65e4b4b651549c3c6ba059cba
ngcdan <linuss1908@gmail.com>
2022-12-20 21:36:45 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './scss/index.scss';
 console.log('call index.js!');
 
-function play() {
-  console.log('call here');
-}
+
+const users = [{ id: '00001', name: "Le Ngoc Dan" }, { id: '12340', name: "Luong Van Dat" }];
 
 const playBtn = document.getElementById('spin');
 playBtn.onclick = function () {
@@ -11,17 +10,24 @@ playBtn.onclick = function () {
   imgAwardEle.style = "display: none";
   const gifEle = document.getElementById('gif');
   gifEle.style = "display: flex";
+  const textNumbers = document.getElementsByClassName('card-text')
 
   // var audio = new Audio('https://youtu.be/zAoroV6Dgdc');
   // audio.play();
 
   setTimeout(function () {
+    let result = "";
     imgAwardEle.style = "";
     gifEle.style = "display: none";
-    const textNumbers = document.getElementsByClassName('card-text');
     for (let i = 0; i < textNumbers.length; i++) {
       const number = Math.floor(Math.random() * 10);
       textNumbers[i].innerHTML = number;
+      result = result + number;
+    }
+    alert(result);
+    const winner = users.find(sel => sel.id == result);
+    if (winner) {
+      console.log(winner.name);
     }
   }, 5000);
 }
